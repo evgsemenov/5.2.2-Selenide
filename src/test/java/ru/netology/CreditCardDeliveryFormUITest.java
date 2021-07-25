75d76f130054e55192a726696a363b773f9d899b
Evgenii Semenov <eo.semenoff@gmail.com>
2021-07-25 21:24:40 +0300

    Positive check added

--- src/test/java/ru/netology/CreditCardDeliveryFormUITest.java
+++ src/test/java/ru/netology/CreditCardDeliveryFormUITest.java
@@ -1,19 +1,25 @@
 package ru.netology;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.text.DateFormat;
+import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CreditCardDeliveryFormUITest {
 
@@ -29,47 +35,35 @@ public class CreditCardDeliveryFormUITest {
 
     @Test
     void shouldPlaceOrderPositiveTest() {
-        $("[data-test-id='city']").setValue("Санкт-Петербург");
-        $("[data-test-id='name']").setValue("Михаил Салтыков-Щедрин");
-        $("[data-test-id='phone']").setValue("+70123456789");
-        $("[data-test-id='agreement']").click();
-        Calendar instance = Calendar.getInstance();
-        instance.add(Calendar.DAY_OF_MONTH, 3);
-        Date newDate = instance.getTime();
-        DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
-        String dateText = dateFormat.format(instance);
-        $("[data-test-id='date']").setValue(dateText);
-        $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
-    }
-
-    @Test
-    void shouldPlaceOrderPositiveV2Test() {
-        $("[placeholder='Город']").setValue("Санкт-Петербург");
-        $("[name='name']").setValue("Михаил Салтыков-Щедрин");
-        $("[name='phone']").setValue("+70123456789");
-        $("[data-test-id='agreement']").click();
-        Calendar instance = Calendar.getInstance();
-        instance.add(Calendar.DAY_OF_MONTH, 3);
-        Date newDate = instance.getTime();
-        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        String dateText = dateFormat.format(instance);      // Дата не форматируется в String
-        $("[placeholder='Дата встречи']").setValue(dateText);
-        $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
-    }
-
-    @Test
-    void shouldPlaceOrderPositiveV3Test() {
-        $("[placeholder='Город']").setValue("Санкт-Петербург");
-        $("[name='name']").setValue("Михаил Салтыков-Щедрин");
-        $("[name='phone']").setValue("+70123456789");
-        $("[data-test-id='agreement']").click();
+        $("[data-test-id='city']").$("[placeholder='Город']").setValue("Санкт-Петербург");
         LocalDate localDate = LocalDate.now();
         LocalDate newDate = localDate.plusDays(3);
-        String dateText = new SimpleDateFormat("dd.MM.yyyy").format(newDate);   // Дата не форматируется в String
-        $("[placeholder='Дата встречи']").setValue(dateText);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateText = newDate.format(formatter);
+        $("[data-test-id='date']").$("[placeholder='Дата встречи']").setValue(dateText);
+        $("[data-test-id= 'name']").$("[name ='name']").setValue("Михаил Салтыков-Щедрин");
+        $("[data-test-id='phone']").$("[name='phone']").setValue("+70123456789");
+        $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
         $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
     }
+
+//    @Test
+//    void shouldRequiredValidCityIfCityNotIncludeTest() {
+//        $("[data-test-id='city']").$("[placeholder='Город']").setValue("Вашингтон");
+//        LocalDate localDate = LocalDate.now();
+//        LocalDate newDate = localDate.plusDays(3);
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+//        String dateText = newDate.format(formatter);
+//        $("[data-test-id='date']").$("[placeholder='Дата встречи']").setValue(dateText);
+//        $("[data-test-id= 'name']").$("[name ='name']").setValue("Михаил Салтыков-Щедрин");
+//        $("[data-test-id='phone']").$("[name='phone']").setValue("+70123456789");
+//        $("[data-test-id='agreement']").click();
+//        $$("button").find(exactText("Забронировать")).click();
+//        String expected = "Доставка в выбранный город недоступна";
+//        String actual = $("[data-test-id='city']").$("[input__sub]").getText();
+//        assertEquals(expected.trim(), actual.trim());
+//    }
 }
+
+
